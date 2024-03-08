83bbd2c234927b6a36c9607f36950f33e6aa969d
RAMIII-pc <morgan48@students.rowan.edu>
2024-03-08 11:08:09 -0500

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,154 @@
+package foxes_rabbits_v2;
+
+import java.util.List;
+import java.util.random;
+
+/**
+ * A class representing shared characteristics of animals.
+ * 
+ * @author David J. Barnes and Michael KÃ¶lling
+ * @version 2011.07.31
+ */
+public abstract class Animal
+{
+    // Whether the animal is alive or not.
+    private boolean alive;
+    // The animal's field.
+    private Field field;
+    // The animal's position in the field.
+    private Location location;
+    // The animal's age.
+    private int age;
+    
+    /**
+     * Create a new animal at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Animal(Field field, Location location)
+    {
+        alive = true;
+        this.field = field;
+        setLocation(location);
+        this.age = 0;
+    }
+    
+    /**
+     * Make this animal act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newAnimals A list to receive newly born animals.
+     */
+    abstract public void act(List<Animal> newAnimals);
+
+    /**
+     * Check whether the animal is alive or not.
+     * @return true if the animal is still alive.
+     */
+    protected boolean isAlive()
+    {
+        return alive;
+    }
+
+    /**
+     * Indicate that the animal is no longer alive.
+     * It is removed from the field.
+     */
+    protected void setDead()
+    {
+        alive = false;
+        if(location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+
+    /**
+     * Return the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Place the animal at the new location in the given field.
+     * @param newLocation The animal's new location.
+     */
+    protected void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+    
+    /**
+     * Return the animal's field.
+     * @return The animal's field.
+     */
+    protected Field getField()
+    {
+        return field;
+    }
+
+    public abstract int getMaxAge();
+
+    protected abstract double getBreedingProbability();
+    
+    protected abstract int getMaxLitterSize();
+
+    /**
+     * Return the animal's age.
+     * @return The animal's age.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set the animal's age.
+     * @param age The animal's new age.
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * An animal can breed if it has reached the breeding age.
+     * @return true if the animal can breed, false otherwise.
+     */
+    protected boolean canBreed()
+    {
+        return age >= getBreedingAge();
+    }
+
+    /**
+     * Increase the age. 
+     * This could result in the animal's death.
+     */
+    protected void incrementAge()
+    {
+        age++;
+        if(age > getMaxAge()) {
+            setDead();
+        }
+    }
+
+    /**
+     * Generate a number representing the number of births,
+     * if it can breed.
+     * @return The number of births (may be zero).
+     */
+    protected int breed()
+    {
+        int births = 0;
+        if(canBreed() && rand.nextDouble() <= getBreedingProbability()) {
+            births = rand.nextInt(getMaxLitterSize()) + 1;
+        }
+        return births;
+    }
+}

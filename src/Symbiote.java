4ad743eea0dbca861914bd4190c605c0856536c7
Michael <mvarrella@comcast.net>
2024-03-12 17:10:46 -0400

    Added instance variables and constructor

--- src/Symbiote.java
+++ src/Symbiote.java
@@ -2,6 +2,20 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.Random;
 
-public class Symbiote {
+public class Symbiote extends Animal{
+
+    private static final int    BREEDING_AGE         = 1;
+    private static final int    MAX_AGE              = 500;
+    private static final double BREEDING_PROBABILITY = .5;
+    private static final int    MAX_LITTER_SIZE      = 1;
+    private static final Random rand                 = Randomizer.getRandom();
+
+    public Symbiote(boolean randomAge, Field field, Location location){
+        super(field, location);
+        setAge(0);
+        if(randomAge){
+            setAge(rand.nextInt(MAX_AGE));
+        }
+    }
 
 }
